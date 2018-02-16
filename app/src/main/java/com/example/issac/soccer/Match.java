5cb51c7d4761fc3e6a0d701ca9bfecbc950b3407
Issac Arzate Soto <issac@MacBook-Pro-de-Issac.local>
2018-02-16 10:55:05 -0600

    first commit

---
+++ app/src/main/java/com/example/issac/soccer/Match.java
@@ -0,0 +1,10 @@
+package com.example.issac.soccer;
+
+/**
+ * Created by issac on 13/02/18.
+ */
+
+public class Match {
+    public String fecha, equipo01, equipo02;
+    public Integer marcador01, marcador02;
+}
